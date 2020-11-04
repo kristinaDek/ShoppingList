d3286f2f9d86f7425d30fd361c5231225ead627c
KristinaDek <kristina.dekic8@gmail.com>
2020-11-04 09:49:13 +0100

    added menu, log out, guard, etc.

--- src/app/auth/log-in/log-in.page.ts
+++ src/app/auth/log-in/log-in.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth.service';
 import {Router} from '@angular/router';
-import {URL} from 'url';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -18,8 +17,11 @@ export class LogInPage implements OnInit {
 
   onLogIn(logInForm: NgForm){
     console.log(logInForm);
-    // this.authService.logIn();
-    // this.router.navigateByUrl( '/home');
+    if (logInForm.valid)
+    {
+      this.authService.logIn();
+      this.router.navigateByUrl( '/home');
+    }
   }
 
 }
